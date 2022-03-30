8298b14f7d94e8b22501fe094eea51e4a960c942
Riku Vartiainen <vartiainenriku@gmail.com>
2022-03-30 16:26:06 +0300

    updates

--- src/main/java/com/example/WebSecurityConfig.java
+++ src/main/java/com/example/WebSecurityConfig.java
@@ -3,7 +3,6 @@ package com.example;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
